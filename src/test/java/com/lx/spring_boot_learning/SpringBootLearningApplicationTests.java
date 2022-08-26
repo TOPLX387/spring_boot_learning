d7ab83c377cffc5e9c62a17b5f80265d481a7600
lx <2564731769@qq.com>
2022-08-26 11:18:33 +0800

    Initial commit

---
+++ src/test/java/com/lx/spring_boot_learning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx.spring_boot_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
